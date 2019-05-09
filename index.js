98bd2ad9c6f95f45bbd2140e1b6add78bd2411f1
github username <github email address>
2019-05-09 13:46:05 -0400

    Done.

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ class Driver {
   trips(){
     return store.trips.filter(
         function(trip) {
-           debugger
             return trip.driverId === this.id;
         }.bind(this)
     );
   }
+  passengers(){
+     return this.trips().map(function(trip){
+       return trip.passenger();
+    });
+  }
 
 }
-
 
 class Passenger{
   constructor(name){
@@ -30,9 +33,16 @@ class Passenger{
     store.passengers.push(this)
   }
   trips(){
-
+    return store.trips.filter(
+        function(trip) {
+            return trip.passengerId === this.id;
+        }.bind(this)
+    );
   }
   drivers(){
+     return this.trips().map(function(trip){
+      return trip.driver();
+   });
 
   }
 }
@@ -41,7 +51,22 @@ class Trip{
   constructor(driver, passenger){
     this.id = ++tripId
     this.driverId = driver.id;
+    this.passengerId = passenger.id;
     store.trips.push(this)
   }
+  passenger(){
+    return store.passengers.find(
+          function(passenger) {
+              return passenger.id === this.passengerId;
+          }.bind(this)
+      );
 
+  }
+  driver(){
+    return store.drivers.find(
+      function(driver){
+         return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
 }
